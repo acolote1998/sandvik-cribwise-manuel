640db64c1919b70e034e0a9ee0f5ee13bf05335d
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-29 11:02:28 +0200

    defining data array for sandvik navbar items

---
+++ src/util/sandvikNavBarItems.ts
@@ -0,0 +1,28 @@
+import { SandvikNavBarItem } from './types';
+
+export const sandvikNavBarItems: SandvikNavBarItem[] = [
+  {
+    id: 0,
+    name: 'Offerings',
+  },
+  {
+    id: 1,
+    name: 'Investors',
+  },
+  {
+    id: 2,
+    name: 'Careers',
+  },
+  {
+    id: 3,
+    name: 'News & Media',
+  },
+  {
+    id: 4,
+    name: 'Stories',
+  },
+  {
+    id: 5,
+    name: 'About us',
+  },
+];
