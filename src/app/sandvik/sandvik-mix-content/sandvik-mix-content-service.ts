edad8fa03cb574e1471f79693cd37b1174b16144
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-29 18:06:32 +0200

    mix content service

--- src/app/sandvik/sandvik-mix-content/sandvik-mix-content-service.ts
+++ src/app/sandvik/sandvik-mix-content/sandvik-mix-content-service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { mixContentItems } from '../../../util/sandvikMixContentItems';
 
 @Injectable({ providedIn: 'root' })
-export class SandvikMixContentService {}
+export class SandvikMixContentService {
+  get getMixContentItems() {
+    return mixContentItems;
+  }
+}
