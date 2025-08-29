df9d560579bb6df8a07cc77e369994e9e4a7daac
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-29 11:12:33 +0200

    renaming some components for convention and tidyness

--- src/app/sandvik-header/header.ts
+++ src/app/sandvik-header/header.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HeaderNavBar } from '../header-nav-bar/header-nav-bar';
+import { HeaderNavBar } from '../sandvik-header-nav-bar/header-nav-bar';
 
 @Component({
   selector: 'app-header',
--- src/app/sandvik-landing-page/sandvik-landing-page.ts
+++ src/app/sandvik-landing-page/sandvik-landing-page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Header } from '../header/header';
+import { Header } from '../sandvik-header/header';
 
 @Component({
   selector: 'app-sandvik-landing-page',
