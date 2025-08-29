460011b85a76089f889389047b751e86ddb67a7a
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-29 15:42:19 +0200

    injecting news service in news component

--- src/app/sandvik-news-component/sandvik-news-component.ts
+++ src/app/sandvik-news-component/sandvik-news-component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { SandvikNewsService } from './sandvik-news-service';
 
 @Component({
   selector: 'app-sandvik-news-component',
   imports: [],
   templateUrl: './sandvik-news-component.html',
-  styles: ``
+  styles: ``,
 })
 export class SandvikNewsComponent {
-
+  newsService: SandvikNewsService;
+  constructor(newsService: SandvikNewsService) {
+    this.newsService = newsService;
+  }
 }
