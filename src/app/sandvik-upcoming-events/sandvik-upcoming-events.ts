cc7fcc570f1ebece88ba69b554b2ae98f10ad14e
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-29 21:29:36 +0200

    upcoming events component

---
+++ src/app/sandvik-upcoming-events/sandvik-upcoming-events.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sandvik-upcoming-events',
+  imports: [],
+  templateUrl: './sandvik-upcoming-events.html',
+  styles: ``
+})
+export class SandvikUpcomingEvents {
+
+}
