e8c3d4129ab3beb8eb2ad04ff28d940d9669351d
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-29 15:41:34 +0200

    adding news service

---
+++ src/app/sandvik-news-component/sandvik-news-service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { newsItems } from '../../util/sandvikNewsItems';
+@Injectable({ providedIn: 'root' })
+export class SandvikNewsService {
+  get getNewsItems() {
+    return newsItems;
+  }
+}
