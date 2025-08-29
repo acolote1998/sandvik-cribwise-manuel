50994bffc0fec72e63f56672d378365f1383d6da
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-29 13:34:22 +0200

    adding 'Welcome to the Sandvik Group' text to header content

---
+++ src/app/sandvik-header-content/sandvik-header-content.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sandvik-header-content',
+  imports: [],
+  templateUrl: './sandvik-header-content.html',
+  styles: ``
+})
+export class SandvikHeaderContent {
+
+}
--- src/app/sandvik-header/header.ts
+++ src/app/sandvik-header/header.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderNavBar } from '../sandvik-header-nav-bar/header-nav-bar';
 import { SandvikNavBarSideBar } from '../sandvik-nav-bar-side-bar/sandvik-nav-bar-side-bar';
+import { SandvikHeaderContent } from '../sandvik-header-content/sandvik-header-content';
 
 @Component({
   selector: 'app-header',
-  imports: [HeaderNavBar, SandvikNavBarSideBar],
+  imports: [HeaderNavBar, SandvikNavBarSideBar, SandvikHeaderContent],
   templateUrl: './header.html',
   styles: ``,
 })
