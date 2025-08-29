78be92f92404ad55c9fb82c794414113add98355
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-29 18:07:26 +0200

    using mix content service in mix content component

--- src/app/sandvik/sandvik-mix-content/sandvik-mix-content.ts
+++ src/app/sandvik/sandvik-mix-content/sandvik-mix-content.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { SandvikMixContentService } from './sandvik-mix-content-service';
 
 @Component({
   selector: 'app-sandvik-mix-content',
   imports: [],
   templateUrl: './sandvik-mix-content.html',
-  styles: ``
+  styles: ``,
 })
 export class SandvikMixContent {
-
+  mixContentService: SandvikMixContentService;
+  constructor(mixContentService: SandvikMixContentService) {
+    this.mixContentService = mixContentService;
+  }
 }
