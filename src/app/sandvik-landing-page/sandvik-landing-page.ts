2f9ca22cea46cfb1d3ab0d63cec579fcce194ca5
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-29 10:21:34 +0200

    creating header component with image + adding favicon

---
+++ src/app/sandvik-landing-page/sandvik-landing-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Header } from '../header/header';
+
+@Component({
+  selector: 'app-sandvik-landing-page',
+  imports: [Header],
+  templateUrl: './sandvik-landing-page.html',
+  styles: ``,
+})
+export class SandvikLandingPage {}
