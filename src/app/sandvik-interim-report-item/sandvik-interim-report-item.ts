3a314867e021671d7a7a08de4b7ad8d107a90827
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-29 14:57:21 +0200

    rendering interim report items dynamically

---
+++ src/app/sandvik-interim-report-item/sandvik-interim-report-item.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { SandvikInterimItemType } from '../../util/types';
+
+@Component({
+  selector: 'app-sandvik-interim-report-item',
+  imports: [],
+  templateUrl: './sandvik-interim-report-item.html',
+})
+export class SandvikInterimReportItem {
+  @Input({ required: true }) itemData!: SandvikInterimItemType;
+}
--- src/app/sandvik-interim-report-section/sandvik-interim-report-section.ts
+++ src/app/sandvik-interim-report-section/sandvik-interim-report-section.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { SandvikInterimService } from './sandvik-interim-service';
+import { SandvikInterimReportItem } from '../sandvik-interim-report-item/sandvik-interim-report-item';
 
 @Component({
   selector: 'app-sandvik-interim-report-section',
-  imports: [],
+  imports: [SandvikInterimReportItem],
   templateUrl: './sandvik-interim-report-section.html',
-  styles: ``
+  styles: ``,
 })
 export class SandvikInterimReportSection {
-
+  interimService: SandvikInterimService;
+  constructor(interimService: SandvikInterimService) {
+    this.interimService = interimService;
+  }
 }
