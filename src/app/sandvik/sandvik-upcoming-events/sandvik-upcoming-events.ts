b695f67ece518ca41ba9be63aa2cd8a74a67f34a
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-29 21:38:32 +0200

    using upcoming events service in component

--- src/app/sandvik/sandvik-upcoming-events/sandvik-upcoming-events.ts
+++ src/app/sandvik/sandvik-upcoming-events/sandvik-upcoming-events.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { SandvikUpcomingEventsService } from './sandvik-upcoming-events-service';
 
 @Component({
   selector: 'app-sandvik-upcoming-events',
   imports: [],
   templateUrl: './sandvik-upcoming-events.html',
-  styles: ``
+  styles: ``,
 })
 export class SandvikUpcomingEvents {
-
+  upcomingEventsService: SandvikUpcomingEventsService;
+  constructor(upcomingEventsService: SandvikUpcomingEventsService) {
+    this.upcomingEventsService = upcomingEventsService;
+  }
 }
