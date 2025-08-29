21e54908525f2c8392fb2d44eb2f6daf8e4dcd33
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-29 10:58:09 +0200

    autowiring navbar service and the navbar

--- src/app/header-nav-bar/header-nav-bar.ts
+++ src/app/header-nav-bar/header-nav-bar.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderNavBarService } from './header-nav-bar-service';
 
 @Component({
   selector: 'app-header-nav-bar',
   imports: [],
   templateUrl: './header-nav-bar.html',
-  styles: ``
+  styles: ``,
 })
 export class HeaderNavBar {
-
+  constructor(private navBarService: HeaderNavBarService) {
+    this.navBarService = navBarService;
+  }
 }
