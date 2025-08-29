873216e414206b9e361aa4484bbd37f36468b0d1
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-29 11:11:17 +0200

    in the sandvik nav bar we render custom item component

--- src/app/header-nav-bar/header-nav-bar.ts
+++ src/app/header-nav-bar/header-nav-bar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderNavBarService } from './header-nav-bar-service';
+import { SandvikNavBarItemComponent } from '../sandvik-nav-bar-item-component/sandvik-nav-bar-item-component';
 
 @Component({
   selector: 'app-header-nav-bar',
-  imports: [],
+  imports: [SandvikNavBarItemComponent],
   templateUrl: './header-nav-bar.html',
   styles: ``,
 })
---
+++ src/app/sandvik-nav-bar-item-component/sandvik-nav-bar-item-component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { SandvikNavBarItem } from '../../util/types';
+
+@Component({
+  selector: 'app-sandvik-nav-bar-item-component',
+  imports: [],
+  templateUrl: './sandvik-nav-bar-item-component.html',
+  styles: ``,
+})
+export class SandvikNavBarItemComponent {
+  @Input({ required: true }) itemData!: SandvikNavBarItem;
+}
