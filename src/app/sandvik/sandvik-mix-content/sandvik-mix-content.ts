868942163ceb5566fbcc913a041dffa54bde627f
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-29 17:55:26 +0200

    creating mix content component

---
+++ src/app/sandvik/sandvik-mix-content/sandvik-mix-content.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sandvik-mix-content',
+  imports: [],
+  templateUrl: './sandvik-mix-content.html',
+  styles: ``
+})
+export class SandvikMixContent {
+
+}
