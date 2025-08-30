d820fe4cc150b864e7b1dc5d67293e19892ade31
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-30 16:39:44 +0200

    routing back to index from navbar in demo

--- src/app/cribwise/cribwise-demo-landing-page/cribwise-demo-landing-page.ts
+++ src/app/cribwise/cribwise-demo-landing-page/cribwise-demo-landing-page.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cribwise-demo-landing-page',
   imports: [],
   templateUrl: './cribwise-demo-landing-page.html',
-  styles: ``
+  styles: ``,
 })
 export class CribwiseDemoLandingPage {
-
+  constructor(private router: Router) {}
+  toIndex() {
+    this.router.navigate(['/cribwise/demo']);
+  }
 }
