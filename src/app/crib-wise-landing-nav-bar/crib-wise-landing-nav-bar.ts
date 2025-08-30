3fa87c0befab66daae5a1ad1ca7801709f8994c2
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-30 15:10:30 +0200

    creating custom component for cribwise landing navbar

---
+++ src/app/crib-wise-landing-nav-bar/crib-wise-landing-nav-bar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crib-wise-landing-nav-bar',
+  imports: [],
+  templateUrl: './crib-wise-landing-nav-bar.html',
+  styles: ``
+})
+export class CribWiseLandingNavBar {
+
+}
--- src/app/cribwise/crib-wise-landing-page/crib-wise-landing-page.ts
+++ src/app/cribwise/crib-wise-landing-page/crib-wise-landing-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CribWiseLandingNavBar } from '../../crib-wise-landing-nav-bar/crib-wise-landing-nav-bar';
 
 @Component({
   selector: 'app-crib-wise-landing-page',
-  imports: [],
+  imports: [CribWiseLandingNavBar],
   templateUrl: './crib-wise-landing-page.html',
   styles: ``,
 })
