b033ada66c2d2e52e46d29bbe868630d7fffabd8
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-30 10:40:35 +0200

    adding page links to the footer

--- src/app/sandvik/sandvik-footer-section/sandvik-footer-section.ts
+++ src/app/sandvik/sandvik-footer-section/sandvik-footer-section.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { SandvikFooterService } from './sandvik-footer-service';
 
 @Component({
   selector: 'app-sandvik-footer-section',
   imports: [],
   templateUrl: './sandvik-footer-section.html',
-  styles: ``
+  styles: ``,
 })
 export class SandvikFooterSection {
-
+  footerService: SandvikFooterService;
+  constructor(footerService: SandvikFooterService) {
+    this.footerService = footerService;
+  }
 }
