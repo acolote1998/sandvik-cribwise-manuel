86274055705de4c1bf2c0255753689d17090276c
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-30 18:25:50 +0200

    table in machines page

--- src/app/cribwise/cribwise-demo-machines-page/cribwise-demo-machines-page.ts
+++ src/app/cribwise/cribwise-demo-machines-page/cribwise-demo-machines-page.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { CribwiseDemoHeader } from '../cribwise-demo-header/cribwise-demo-header';
 import { CribwiseDemoNavBar } from '../cribwise-demo-nav-bar/cribwise-demo-nav-bar';
+import { MachinesService } from './cribwise-demo-machines-service';
 
 @Component({
   selector: 'app-cribwise-demo-machines-page',
   imports: [CribwiseDemoHeader, CribwiseDemoNavBar],
   templateUrl: './cribwise-demo-machines-page.html',
 })
-export class CribwiseDemoMachinesPage {}
+export class CribwiseDemoMachinesPage {
+  machinesService: MachinesService;
+  constructor(machinesService: MachinesService) {
+    this.machinesService = machinesService;
+  }
+}
