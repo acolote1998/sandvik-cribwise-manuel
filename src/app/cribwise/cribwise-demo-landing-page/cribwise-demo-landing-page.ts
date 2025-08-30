9ae1f1e87bb0f83a997ee33bf0325324a10047a7
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-30 17:08:43 +0200

    cribwise demo navbar first block

--- src/app/cribwise/cribwise-demo-landing-page/cribwise-demo-landing-page.ts
+++ src/app/cribwise/cribwise-demo-landing-page/cribwise-demo-landing-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CribwiseDemoHeader } from '../cribwise-demo-header/cribwise-demo-header';
+import { CribwiseDemoNavBar } from '../cribwise-demo-nav-bar/cribwise-demo-nav-bar';
 
 @Component({
   selector: 'app-cribwise-demo-landing-page',
-  imports: [CribwiseDemoHeader],
+  imports: [CribwiseDemoHeader, CribwiseDemoNavBar],
   templateUrl: './cribwise-demo-landing-page.html',
   styles: ``,
 })
---
+++ src/app/cribwise/cribwise-demo-nav-bar/cribwise-demo-nav-bar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cribwise-demo-nav-bar',
+  imports: [],
+  templateUrl: './cribwise-demo-nav-bar.html',
+  styles: ``
+})
+export class CribwiseDemoNavBar {
+
+}
