792e8ddd6c958c4c6b58529b259d2825c7d7d9aa
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-31 18:31:12 +0200

    adding mobile disclaimer to cribwise

--- src/app/cribwise/crib-wise-landing-page/crib-wise-landing-page.ts
+++ src/app/cribwise/crib-wise-landing-page/crib-wise-landing-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CribWiseLandingNavBar } from '../crib-wise-landing-nav-bar/crib-wise-landing-nav-bar';
 import { Router } from '@angular/router';
+import { SandvikMobileDisclaimer } from '../../sandvik/sandvik-mobile-disclaimer/sandvik-mobile-disclaimer';
 
 @Component({
   selector: 'app-crib-wise-landing-page',
-  imports: [CribWiseLandingNavBar],
+  imports: [CribWiseLandingNavBar, SandvikMobileDisclaimer],
   templateUrl: './crib-wise-landing-page.html',
   styles: ``,
 })
--- src/app/cribwise/cribwise-demo-landing-page/cribwise-demo-landing-page.ts
+++ src/app/cribwise/cribwise-demo-landing-page/cribwise-demo-landing-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CribwiseDemoHeader } from '../cribwise-demo-header/cribwise-demo-header';
 import { CribwiseDemoNavBar } from '../cribwise-demo-nav-bar/cribwise-demo-nav-bar';
+import { SandvikMobileDisclaimer } from '../../sandvik/sandvik-mobile-disclaimer/sandvik-mobile-disclaimer';
 
 @Component({
   selector: 'app-cribwise-demo-landing-page',
-  imports: [CribwiseDemoHeader, CribwiseDemoNavBar],
+  imports: [CribwiseDemoHeader, CribwiseDemoNavBar, SandvikMobileDisclaimer],
   templateUrl: './cribwise-demo-landing-page.html',
   styles: ``,
 })
