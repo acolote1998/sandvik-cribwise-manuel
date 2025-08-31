c4300ebda370c4b7b888f3364168c40f6e45c467
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-31 16:26:47 +0200

    adding an error toast

---
+++ src/app/util/error-toast/error-toast.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-toast',
+  imports: [],
+  templateUrl: './error-toast.html',
+  styles: ``,
+})
+export class ErrorToast {}
