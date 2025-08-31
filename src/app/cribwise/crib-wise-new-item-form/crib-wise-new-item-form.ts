c221ef23d06b5aaf5902af693885ec274373f261
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-31 15:07:16 +0200

    adding 2 way bindings to inputs in new item form

--- src/app/cribwise/crib-wise-new-item-form/crib-wise-new-item-form.ts
+++ src/app/cribwise/crib-wise-new-item-form/crib-wise-new-item-form.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
 import { CribwiseDemoHeader } from '../cribwise-demo-header/cribwise-demo-header';
 import { CribwiseDemoNavBar } from '../cribwise-demo-nav-bar/cribwise-demo-nav-bar';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-crib-wise-new-item-form',
-  imports: [CribwiseDemoHeader, CribwiseDemoNavBar],
+  imports: [CribwiseDemoHeader, CribwiseDemoNavBar, FormsModule],
   templateUrl: './crib-wise-new-item-form.html',
   styles: ``,
 })
-export class CribWiseNewItemForm {}
+export class CribWiseNewItemForm {
+  itemName!: string;
+  newQty!: number;
+  usedQty!: number;
+  refurbQty!: number;
+  forServiceQty!: number;
+  pickedQty!: number;
+  atLocationQty!: number;
+  maxQty!: number;
+  orderPointQty!: number;
+  minQty!: number;
+  criticalLevelQty!: number;
+  missingToMaxQty!: number;
+  state!: boolean;
+  activeOrdersQty!: number;
+  expectFromOrdersQty!: number;
+  expetedDateOfDelivery!: string;
+
+  calculateState(availability: string) {
+    if (availability === 'available') {
+      return true;
+    } else return false;
+  }
+}
