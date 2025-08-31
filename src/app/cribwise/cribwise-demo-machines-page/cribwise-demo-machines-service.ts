f19537b070c4419efec09c0dcefccb2a05ad7c6f
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-31 15:14:02 +0200

    adding methods to machine and tool services to add new tools

--- src/app/cribwise/cribwise-demo-machines-page/cribwise-demo-machines-service.ts
+++ src/app/cribwise/cribwise-demo-machines-page/cribwise-demo-machines-service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 import { machinesData } from '../../../util/warehouseItems';
+import type { CribwiseWarehouseItemType } from '../../../util/types';
 
 @Injectable({ providedIn: 'root' })
 export class MachinesService {
   get getMachinesData() {
     return machinesData;
+  }
+  addNewTool(machine: CribwiseWarehouseItemType) {
+    machinesData.push(machine);
   }
 }
--- src/app/cribwise/cribwise-demo-tools-page/cribwise-demo-tools-service.ts
+++ src/app/cribwise/cribwise-demo-tools-page/cribwise-demo-tools-service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 import { toolsData } from '../../../util/warehouseItems';
+import { CribwiseWarehouseItemType } from '../../../util/types';
 
 @Injectable({ providedIn: 'root' })
 export class ToolsService {
   get getToolsData() {
     return toolsData;
+  }
+  addNewTool(tool: CribwiseWarehouseItemType) {
+    toolsData.push(tool);
   }
 }
