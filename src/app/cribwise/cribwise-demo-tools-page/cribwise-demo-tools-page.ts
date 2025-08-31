d57b72ac21df999aa6c19fc6a698579bbcaa6f1b
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-31 11:40:39 +0200

    updating tools page styling

--- src/app/cribwise/cribwise-demo-tools-page/cribwise-demo-tools-page.ts
+++ src/app/cribwise/cribwise-demo-tools-page/cribwise-demo-tools-page.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { CribwiseDemoHeader } from '../cribwise-demo-header/cribwise-demo-header';
 import { CribwiseDemoNavBar } from '../cribwise-demo-nav-bar/cribwise-demo-nav-bar';
+import { CribWiseDemoTableForItems } from '../crib-wise-demo-table-for-items/crib-wise-demo-table-for-items';
+import { ToolsService } from './cribwise-demo-tools-service';
 
 @Component({
   selector: 'app-cribwise-demo-tools-page',
-  imports: [CribwiseDemoHeader, CribwiseDemoNavBar],
+  imports: [CribwiseDemoHeader, CribwiseDemoNavBar, CribWiseDemoTableForItems],
   templateUrl: './cribwise-demo-tools-page.html',
 })
-export class CribwiseDemoToolsPage {}
+export class CribwiseDemoToolsPage {
+  toolsService: ToolsService;
+  constructor(toolsService: ToolsService) {
+    this.toolsService = toolsService;
+  }
+}
